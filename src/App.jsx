f94057ae4a1a8af7923b887354c135832a032569
Emirchelo-Lopez <elopezarmenincome@gmail.com>
2025-07-31 09:30:06 -0600

    Migrated routings from App to AppRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import "./assets/styles/main.scss";
-import HomePage from "./pages/HomePage/HomePage";
-import ChatPage from "./pages/ChatPage/ChatPage";
-import NotFoundPage from "./pages/NotFound/NotFound";
-import LandingPage from "./pages/LandingPage/LandingPage";
-import SettingsPage from "./pages/SettingsPage/SettingsPage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import SignupPage from "./pages/SignupPage/SignupPage";
+import AppRouter from "./routes/AppRouter";
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        {/* <HomePage /> */}
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </Router>
-  );
+  return <AppRouter></AppRouter>;
 };
 
 export default App;
---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "../pages/HomePage/HomePage";
+import ChatPage from "../pages/ChatPage/ChatPage";
+import NotFoundPage from "../pages/NotFound/NotFound";
+import LandingPage from "../pages/LandingPage/LandingPage";
+import SettingsPage from "../pages/SettingsPage/SettingsPage";
+import LoginPage from "../pages/LoginPage/LoginPage";
+import SignupPage from "../pages/SignupPage/SignupPage";
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* <HomePage /> */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
