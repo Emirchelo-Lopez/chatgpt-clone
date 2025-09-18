252178bfd3d4671a84cf0f6ad4c2193363ea6d1c
Emirchelo-Lopez <elopezarmenincome@gmail.com>
2025-09-17 20:47:58 -0600

    🔧 refactor: Replace AuthProvider and ChatProvider with AppProvider in main.jsx to streamline context management and simplify the rendering structure.

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { AuthProvider } from "./context/AuthContext.jsx";
-import { ChatProvider } from "./context/ChatContext.jsx";
+import { AppProvider } from "./context/AppProvider.jsx";
 import App from "./App.jsx";
 import "./assets/styles/main.scss";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <ChatProvider>
-        <App />
-      </ChatProvider>
-    </AuthProvider>
+    <AppProvider>
+      <App />
+    </AppProvider>
   </StrictMode>
 );
